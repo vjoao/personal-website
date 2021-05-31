40d992ef2b9f0aa89bb0ca41dee263c8fc3c9384
vjoao <victormota15@gmail.com>
2021-06-01 00:45:51 +0200

    feat: add google fonts plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,14 @@
 export default {
   target: 'static',
-  buildModules: ['@nuxtjs/tailwindcss'],
+  buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
   css: ['~/assets/main.css'],
   router: {
     linkExactActiveClass: 'border-blue-500'
+  },
+  googleFonts: {
+    families: {
+      'Baloo+Tammudu+2': true
+    },
+    display: 'block'
   }
 };
