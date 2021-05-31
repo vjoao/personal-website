000ed5afd986e41d4e8b2256aca0a22f87fafe82
vjoao <victormota15@gmail.com>
2021-06-01 01:48:36 +0200

    feat: analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,13 @@
 export default {
   target: 'static',
   modules: ['@nuxtjs/robots'],
-  buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@nuxtjs/google-analytics'],
   css: ['~/assets/main.css'],
   router: {
     linkExactActiveClass: 'border-blue-500'
+  },
+  googleAnalytics: {
+    id: 'G-552T1HM19S'
   },
   googleFonts: {
     families: {
